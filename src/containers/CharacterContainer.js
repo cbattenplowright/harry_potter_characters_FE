fca2f24bbea455a351af9ddac572651f3014a794
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-07 11:56:04 +0100

    fetch data function written" "

--- src/containers/CharacterContainer.js
+++ src/containers/CharacterContainer.js
@@ -1,7 +1,28 @@
-import CharacterList from "../containers/CharacterList";
+import { useState, useEffect } from "react";
+import CharacterList from "../components/CharacterList";
 
 const CharacterContainer = () => {
-    return <></>;
+    const [characters, setCharacters] = useState([]);
+
+    const fetchCharacters = async () => {
+        const response = await fetch(
+            "https://hp-api.onrender.com/api/characters"
+        );
+        const jsonData = await response.json();
+        console.log(jsonData);
+        setCharacters(jsonData);
+    };
+
+    useEffect(() => {
+        fetchCharacters();
+    }, []);
+
+    return (
+        <>
+            <h1>The world of harry potter characters</h1>
+            <CharacterList />
+        </>
+    );
 };
 
 export default CharacterContainer;
